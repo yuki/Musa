72b949ffd562a64d6576606f662c0bc3b1dbf26b
Yuki (aka Ruben Gomez) <rugoli@gmail.com>
2016-12-20 23:12:56 +0100

    Initial Commit

---
+++ Musa/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Musa
+//
+//  Created by Rubén Gómez Olivencia on 20/12/16.
+//  Copyright © 2016 Rubén Gómez Olivencia. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
