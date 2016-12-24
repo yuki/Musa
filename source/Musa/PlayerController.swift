a103047b4f9695a3b00a724525486fe46140df57
Yuki (aka Ruben Gomez) <rugoli@gmail.com>
2016-12-24 16:33:58 +0100

    Music player works
    But I hacked the collection that is playing. Is not what is selected
    in the songList... but the first approach works :D

--- source/Musa/PlayerController.swift
+++ source/Musa/PlayerController.swift
@@ -15,10 +15,6 @@ import UIKit
 import MediaPlayer
 
 class PlayerController: UIViewController {
-
-    @IBOutlet weak var songImage: UIImageView!
-    @IBOutlet weak var songName: UILabel!
-    @IBOutlet weak var groupName: UILabel!
     
     private var _songInfo = MPMediaItem()
     var songInfo: MPMediaItem {
@@ -29,23 +25,66 @@ class PlayerController: UIViewController {
         }
     }
     
+    func getNowPlayingItem() {
+        songImage.image = musicPlayer.nowPlayingItem?.artwork?.image(at: CGSize(width: 240, height: 240))
+        songName.text = (musicPlayer.nowPlayingItem?.title != nil) ? musicPlayer.nowPlayingItem?.title : ""
+        groupName.text = (musicPlayer.nowPlayingItem?.artist != nil) ? musicPlayer.nowPlayingItem?.artist : ""
+    }
+    
+    @IBOutlet weak var songImage: UIImageView!
+    @IBOutlet weak var songName: UILabel!
+    @IBOutlet weak var groupName: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    var musicPlayer = MPMusicPlayerController.systemMusicPlayer()
+    //var musicCollection = MPMediaItemCollection.init()
+
+    @IBAction func pressPlayButton(_ sender: UIButton) {
+        if musicPlayer.playbackState == MPMusicPlaybackState.playing {
+            musicPlayer.pause()
+            playButton.setTitle("Play", for: UIControlState.normal)
+        } else {
+            musicPlayer.play()
+            playButton.setTitle("Pause", for: UIControlState.normal)
+            
+        }
+    }
+    
+    @IBOutlet weak var prevButton: UIButton!
+    @IBAction func pressPrevButton(_ sender: UIButton) {
+        musicPlayer.skipToPreviousItem()
+        
+        getNowPlayingItem()
+    }
+
+    @IBOutlet weak var nextButton: UIButton!
+    @IBAction func pressNextButton(_ sender: UIButton) {
+        musicPlayer.skipToNextItem()
+        
+        getNowPlayingItem()
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        musicPlayer.play()
         // FIXME: should not be an static CGSIZE!!!
-        songImage.image = songInfo.artwork?.image(at: CGSize(width: 240, height: 240))
-        songName.text = (songInfo.title != nil) ? songInfo.title : ""
-        groupName.text = (songInfo.artist != nil) ? songInfo.artist : ""
+        songImage.image = musicPlayer.nowPlayingItem?.artwork?.image(at: CGSize(width: 240, height: 240))
+        
+        songName.text = (musicPlayer.nowPlayingItem?.title != nil) ? musicPlayer.nowPlayingItem?.title : ""
+        groupName.text = (musicPlayer.nowPlayingItem?.artist != nil) ? musicPlayer.nowPlayingItem?.artist : ""
+        //musicCollection = MPMediaItemCollection.init(items: [songInfo])
+        //musicPlayer.setQueue(with: musicCollection)
+        NotificationCenter.default.addObserver(self, selector: #selector(PlayerController.getNowPlayingItem), name: NSNotification.Name.MPMusicPlayerControllerNowPlayingItemDidChange, object: nil)
+        musicPlayer.beginGeneratingPlaybackNotifications()
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
