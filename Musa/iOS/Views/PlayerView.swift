b41aa2e86cf40f7dc12cc5452b60f19b98358d90
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2021-07-09 20:27:33 +0200

    Few changes in PlayerView
    I think that now the view is ok

--- Musa/iOS/Views/PlayerView.swift
+++ Musa/iOS/Views/PlayerView.swift
@@ -12,59 +12,66 @@ struct PlayerView: View {
     
     var body: some View {
         VStack {
-            Image("cover")
-                .resizable()
-                .scaledToFit()
-                .cornerRadius(15)
-            Image("cover")
-                .resizable()
-                .scaledToFit()
-                .cornerRadius(15)
-//            VStack {
-//                HStack{
-//                    Text("0:00")
-//                    ProgressView(value: 0.2)
-//                    VStack {
-//
-//                        Spacer()
-//                    }
-//                    Text("1:00")
-//                }
-//                .fixedSize(horizontal: false, vertical: true)
-//                Text("Song Title")
-//                    .font(.title)
-//                Text("Artist")
-//                    .foregroundColor(Color.blue)
-//                HStack{
-//                    Spacer()
-//                    Image(systemName: "backward.fill")
-//                    Spacer()
-//                    Image(systemName: "play.fill")
-//                    Spacer()
-//                    Image(systemName: "forward.fill")
-//                    Spacer()
-//                }
-//                HStack{
-//                    Image(systemName: "speaker.fill")
-//                    Slider(
-//                        value: $time,
-//                        in: 0...100,
-//                        step: 1
-//                    )
-//                    Image(systemName: "speaker.wave.2.fill")
-//                }
-//                HStack{
-//                    Image(systemName: "shuffle")
-//                    Spacer()
-//                    Image(systemName: "repeat")
-//                }
-//            }
-//            .fixedSize(horizontal: false, vertical: false)
+            VStack{
+                Image(systemName: "chevron.compact.down").font(.title).padding([.bottom],5)
+                Image("cover")
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(15)
+            }
+            .frame(maxWidth:.infinity, maxHeight:.infinity)
+            
+            VStack {
+                HStack{
+                    Text("0:00")
+                    ProgressView(value: 0.2)
+                    Text("1:00")
+                }
+                .padding(.top)
+                VStack {
+                    Text("Song Title")
+                        .lineLimit(2)
+                        .font(.title)
+                        .padding(.bottom)
+                        .frame(maxWidth: .infinity)
+                    Text("Artist")
+                        .lineLimit(1)
+                        .frame(maxWidth: .infinity)
+                }
+                .frame(maxWidth:.infinity, maxHeight:.infinity)
+                
+                HStack(spacing: 50.0){
+                    Image(systemName: "backward.fill")
+                    Image(systemName: "play.fill")
+                    Image(systemName: "forward.fill")
+                }
+                .padding(.horizontal)
+                .font(.largeTitle)
+                .frame(maxWidth:.infinity, maxHeight:.infinity)
+                
+                HStack{
+                    Image(systemName: "speaker.fill")
+                    Slider(
+                        value: $time,
+                        in: 0...100,
+                        step: 1
+                    )
+                    Image(systemName: "speaker.wave.2.fill")
+                }
+
+                HStack{
+                    Image(systemName: "shuffle")
+                    Spacer()
+                    Image(systemName: "repeat")
+                }
+                .padding(.top)
+                .font(.title)
+            }
+            .frame(maxWidth:.infinity, maxHeight:.infinity)
+            .fixedSize(horizontal: false, vertical: false)
         }
         .statusBar(hidden: true)
-//        .padding(10.0)
-        
-        
+        .padding(10.0)
     }
 }
 
